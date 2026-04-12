004a2fd7c9a87e708c3e0179e6c7a2868cf81ba7
Ahmed-ragab <ahmedarra3@gmail.com>
2026-04-12 14:27:44 +0200

    Cleanup: Remove model testing scripts and finalize v3.0.0

--- test_deepseek.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const axios = require('axios');
-axios.get('https://api.deepseek.com/models', {
-    headers: { 'Authorization': `Bearer ${process.env.DEEPSEEK_API_KEY}` }
-}).then(res => console.table(res.data.data))
-  .catch(err => console.error(err.message));
